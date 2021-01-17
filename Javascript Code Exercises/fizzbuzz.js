3a4d0470c8c48d08c062dce789b96b1fd9094abd
ItCanBeCoded <64056028+ItCanBeCoded@users.noreply.github.com>
2021-01-17 16:15:41 -0600

    fixing directories

---
+++ Javascript Code Exercises/fizzbuzz.js
@@ -0,0 +1,37 @@
+var output = [];
+var count = 1;
+var buzzy = "";
+
+function fizzbuzz(){
+    
+    if ((count%3 === 0) && (count%5 === 0)){
+        buzzy = "fizzbuzz";
+        output.push(buzzy);
+    }
+    if ((count%3 === 0) && (count%5 !== 0)){
+        buzzy = "fizz";
+        output.push(buzzy);
+    }
+     if ((count%3 !== 0) && (count%5 === 0)){
+        buzzy = "buzz";
+        output.push(buzzy);
+    }
+    if ((count%3 !==0) && (count%5 !==0)){
+        output.push(count);
+    }
+
+    count++;
+    console.log(output);
+
+}
+
+for(var j = 0; j < 10; j++){
+fizzbuzz();
+
+}
+
+for(var i = 0; i < 5; i++){
+
+fizzbuzz();
+}
+
