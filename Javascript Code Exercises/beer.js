0290112efb18520723f40e08ecaa45ebcb78b98d
ItCanBeCoded <64056028+ItCanBeCoded@users.noreply.github.com>
2021-01-17 19:36:28 -0600

    beer!

---
+++ Javascript Code Exercises/beer.js
@@ -0,0 +1,12 @@
+function beer(){
+    var bottles = 99;
+    while (bottles > 0){
+        
+        console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer. Take 1 down, pass it around, " + (bottles-1)+ " bottles of beer on the wall.");
+
+        bottles--;
+    }
+
+}
+
+beer();
--- Javascript Code Exercises/fizzbuzz.js
+++ Javascript Code Exercises/fizzbuzz.js
@@ -3,6 +3,8 @@ var count = 1;
 var buzzy = "";
 
 function fizzbuzz(){
+
+    while(count <= 100){
     
     if ((count%3 === 0) && (count%5 === 0)){
         buzzy = "fizzbuzz";
@@ -21,14 +23,20 @@ function fizzbuzz(){
     }
 
     count++;
-    console.log(output);
+   
 
 }
 
-for(var j = 0; j < 16; j++){
+console.log(output);
+
+}
+
 fizzbuzz();
 
-}
+// for(var j = 0; j < 16; j++){
+// fizzbuzz();
+
+// }
 
 
 
