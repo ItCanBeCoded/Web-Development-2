996312bf1b1a374c52ae1c9d184743e4a5cba0b2
ItCanBeCoded <64056028+ItCanBeCoded@users.noreply.github.com>
2021-01-18 15:44:14 -0600

    complete

--- Javascript Code Exercises/fibonacci.js
+++ Javascript Code Exercises/fibonacci.js
@@ -3,7 +3,7 @@ function fibonacciGenerator(n){
     var a = 0;
     var b = 1;
     var printy = true;
-    for(var i = 1; i < n; i++ ){
+    for(var i = 1; i < n-1; i++ ){
         if(n === 1){
     
         }
@@ -24,8 +24,32 @@ function fibonacciGenerator(n){
         
         }
     }
+
+    if(n === 2){
+        fibo.push(1);
+    }
     console.log(fibo)
 
 }
 
-fibonacciGenerator(20);
+fibonacciGenerator(100);
+
+
+function fiboGen2(n){
+    var output = [];
+    if (n === 1){
+        output = [0];
+    }
+    else if (n === 2){
+        output = [0,1];
+    }
+    else {
+        output = [0,1];
+        for (var i = 2; i < n; i++){
+        output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    
+    return output;
+
+}
